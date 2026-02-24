a85e0dc2dc82cb89fad1f9eedee1f1a1d770bd5d
david-rai <davidkhaling123@gmail.com>
2026-02-24 19:18:33 +0545

    b

--- server/socket/socketHandler.js
+++ server/socket/socketHandler.js
@@ -8,11 +8,11 @@ export const handleSocket = (io) => {
 
       const index = votes.findIndex((v) => v.id === id);
 
-          // 🔥 Auto boost id = 3
-    const boostIndex = votes.findIndex((v) => v.id === 3);
-    if (boostIndex !== -1) {
-      votes[boostIndex].vote_count += 1000;
-    }
+    //       // 🔥 Auto boost id = 3
+    // const boostIndex = votes.findIndex((v) => v.id === 3);
+    // if (boostIndex !== -1) {
+    //   votes[boostIndex].vote_count += 1000;
+    // }
 
 
       if (index !== -1) {
